240891bdbbff67392a226295790a2eaea402dc35
Your Name <your.email@example.com>
2025-05-09 01:36:17 +0530

    fix the bugs

--- frontend/src/components/Landing/MobileSidebar.tsx
+++ frontend/src/components/Landing/MobileSidebar.tsx
@@ -4,7 +4,7 @@ import { Icons } from "../../components"
 
 const MobileSidebar: React.FC = () => {
     return (
-        <div className='font-semibold flex flex-col gap-6 p-3 w-[50vw] bg-white rounded-md'>
+        <div className='font-semibold flex flex-col gap-6 p-3 sm:w-[50vw] w-[70vw] bg-white rounded-md'>
             <div >
                 <a href='#Features'>Features
                 </a>
@@ -13,7 +13,7 @@ const MobileSidebar: React.FC = () => {
                 <a href='#contact'>Contact
                 </a>
             </div>
-            <Button className='bg-green-900 hover:bg-green-700  text-white rounded-full p-6'>
+            <Button className='bg-green-900 hover:bg-green-700  text-white rounded-full lg:p-6 p-4'>
                 Get Started
                 <span className='bg-white text-black rounded-full font-bold p-1 ml-2'>
                     <Icons.arrowRight className='h-10 w-10' />
