a9b374ebd6ef43f2a9938a0f3f566e9c4bd9bdc8
Your Name <your.email@example.com>
2025-05-14 15:37:09 +0530

    fix something

---
+++ frontend/src/lib/generateSchema.ts
@@ -0,0 +1,20 @@
+import { useFormBuilderStore } from "../store/frombuilder";
+import * as z from "zod";
+export const generateSchema = () => {
+    const { blocks } = useFormBuilderStore.getState();
+    const shape: any = {};
+
+    blocks.forEach((block) => {
+        const name = `block-${block.id}`;
+        if (block.type === "email") {
+            shape[name] = block.props.required
+                ? z.string().email("Invalid email").min(1, "Required")
+                : z.string().email("Invalid email").optional();
+        }
+        if (block.type === "button") {
+            shape[name] = z.string().optional();
+        }
+    });
+
+    return z.object(shape);
+};
