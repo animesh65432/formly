d93ae4e27709bed36e28699fa90642ed7f7c650f
Your Name <your.email@example.com>
2025-05-14 10:34:35 +0530

    fix the bug

--- frontend/src/components/FromBuilder/FromPreview/index.tsx
+++ frontend/src/components/FromBuilder/FromPreview/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const FromPreview: React.FC = () => {
     return (
-        <div className='col-span-3 bg-slate-100'>FromPreview</div>
+        <div className='col-span-3 bg-slate-100 h-[98vh] scrollbar-custom-x'>
+
+        </div>
     )
 }
 
