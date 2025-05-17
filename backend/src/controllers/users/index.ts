4e5632f7df521219a6ef6cefe475ee7ae30117d8
Your Name <your.email@example.com>
2025-05-17 18:53:18 +0530

    add auth

---
+++ backend/src/controllers/users/index.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express"
+import db from "../../db"
+import bcrypt from "bcrypt"
+import { createToken } from "../../utils"
+import { asyncerrorhandler } from "../../middlewares"
+
+
+const createUser = asyncerrorhandler(async (req: Request, res: Response) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400).json({ message: "All fields are required" })
+        return
+    }
+
+    const exsitingUser = await db.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (exsitingUser) {
+        res.status(400).json({ message: "User already exists" })
+        return
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    res.status(201).json({ message: "User created successfully" })
+    return
+})
+
+const loginUser = asyncerrorhandler(async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        res.status(400).json({ message: "All fields are required" })
+        return
+    }
+
+    const user = await db.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (!user || !user.password) {
+        res.status(400).json({ message: "Invalid credentials" })
+        return
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        res.status(400).json({ message: "Invalid credentials" })
+        return
+    }
+
+    const token = await createToken(user.email)
+
+    res.status(200).json({ message: "Login successful", token })
+    return
+})
+
+export { createUser, loginUser }
---
+++ backend/src/middlewares/rateLimiter.ts
@@ -0,0 +1,55 @@
+import { Request, Response, NextFunction } from "express";
+import db from "../db";
+const RateLimiter = (limit: number, windowMs: number) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const ip = req.ip;
+        const now = Date.now();
+
+        let entry
+
+        try {
+
+            entry = await db.rateLimit.findUnique({ where: { ip } });
+
+            if (!entry && ip) {
+                entry = await db.rateLimit.create({
+                    data: { ip, count: 1, lastRequest: now },
+                });
+
+            }
+
+            const elapsed = entry ? now - Number(entry.lastRequest) : Infinity;
+
+            if (elapsed > windowMs) {
+                await db.rateLimit.update({
+                    where: { ip },
+                    data: { count: 1, lastRequest: now },
+                });
+                next();
+                return
+            }
+
+            if (entry && entry.count < limit) {
+                await db.rateLimit.update({
+                    where: { ip },
+                    data: {
+                        count: { increment: 1 },
+                        lastRequest: now,
+                    },
+                });
+                next();
+                return
+            }
+
+
+            res.status(429).json({ message: "Too many requests." });
+            return
+        } catch (err) {
+            console.error("Rate limiter error:", err);
+            next();
+            return
+        }
+    };
+};
+
+export default RateLimiter;
