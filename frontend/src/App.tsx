84c5210900ff9b6dd9384f3ff4296b100fb958a7
Your Name <your.email@example.com>
2025-05-19 18:15:23 +0530

    fix with the code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Dashboard, LandingPage, SinginPage, BuilderPage } from "./pages"
+import { Dashboard, LandingPage, SinginPage, BuilderPage, SignupPage } from "./pages"
 export default function App() {
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
         <Route path="/singin" element={<SinginPage />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/build" element={<BuilderPage />}></Route>
--- frontend/src/pages/Singin.tsx
+++ frontend/src/pages/Singin.tsx
@@ -4,7 +4,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google"
 import { config } from "../config"
 
 const SinginPage: React.FC = () => {
-    console.log(config.GOOGLE_CLIENT_ID)
     return (
         <GoogleOAuthProvider clientId={config.GOOGLE_CLIENT_ID}>
             <div className='bg-slate-100 w-[100vw]'>
