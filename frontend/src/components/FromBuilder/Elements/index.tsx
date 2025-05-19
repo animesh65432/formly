fa6f4eb007a9ea25f226722f92bb31032084006d
Uzrakhan <uzrakhan539@gmail.com>
2025-05-19 13:14:59 +0530

    Made the buildpage responsive and fixed calender

--- frontend/src/components/FromBuilder/Elements/index.tsx
+++ frontend/src/components/FromBuilder/Elements/index.tsx
@@ -4,10 +4,16 @@ import Element from './Element'
 
 const FormElements: React.FC = () => {
     return (
-        <div className='col-span-1 h-[90vh] bg-white scrollbar-custom-x flex flex-col gap-2 p-3'>
-            {
+        <div className='
+            h-full bg-white scrollbar-custom-x p-3
+               overflow-y-auto
+               md:sticky md:top-0'>
+            <div className='grid grid-cols-3 grid-rows-3 text-xl space-y-1 gap-1 md:grid-cols-1 md:gap-3'>
+                {
                 formElements.map((element) => <Element key={element.id} element={element} />)
-            }
+                }
+            </div>
+            
         </div>
     )
 }
