1144035035f5720619b0b5c77f232c663b05bf8b
Your Name <your.email@example.com>
2025-05-28 13:18:41 +0530

    create th from

--- frontend/src/components/dashboard/index.tsx
+++ frontend/src/components/dashboard/index.tsx
@@ -1,10 +1,10 @@
 import Sidebar from "./Sidebar"
-import React, { useEffect } from "react"
-import { useGetFroms } from "../../actions/from"
+import React from "react"
+// import { useGetFroms } from "../../actions/from"
 import { useAuth } from "../../store/auth"
 const Dashboard: React.FC = () => {
     const { token } = useAuth()
-    const { data } = useGetFroms(token)
+    // const { data } = useGetFroms(token)
     return (
         <div className="bg-slate-100 w-[100vw] h-dvh grid grid-cols-11 gap-5">
             <div className="col-span-2">
