651468a4e6f7e69c569d49b844ae5dd17dfe6a7f
Your Name <your.email@example.com>
2025-05-29 18:47:42 +0530

    add skelton

--- frontend/src/components/FromBuilder/Fromheader/index.tsx
+++ frontend/src/components/FromBuilder/Fromheader/index.tsx
@@ -5,18 +5,25 @@ import { useAuth } from "../../../store/auth"
 import { useFormBuilderStore } from "../../../store/frombuilder"
 import Icons from '../../Icons'
 import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
 
 const Fromheader: React.FC = () => {
     const { token } = useAuth()
     const { block } = useFormBuilderStore()
     const { mutate: create, isPending } = useCreateFrom()
+    const navigate = useNavigate()
 
     const createfrom = async () => {
         await create({ token, block })
         toast.success("sucessfully create from")
     }
+    const goBack = () => {
+        navigate(-1) // ✅ 
+    }
+
     return (
-        <div>
+        <div className='flex gap-4'>
+            <Button className='bg-green-800 rounded-md hover:bg-green-700' onClick={() => goBack()}>{isPending ? <Icons.spinner className='animate-spin w-5 h-5 md:w-6 md:h-6 ' /> : "BACK"}</Button>
             <Button className='bg-green-800 rounded-md hover:bg-green-700' onClick={createfrom}>{isPending ? <Icons.spinner className='animate-spin w-5 h-5 md:w-6 md:h-6 ' /> : "SAVE"}</Button>
         </div>
     )
