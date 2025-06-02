a3f56849791d13dca26406f861327bc2d584a452
Your Name <your.email@example.com>
2025-06-03 00:47:39 +0530

    update intergation

--- frontend/src/components/Intergations/Notion/index.tsx
+++ frontend/src/components/Intergations/Notion/index.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { generateOAuthURL } from "../../../api/Integration/notion"
 import { Button } from "../../ui/button"
 import { useAuth } from '../../../store/auth'
+import Icons from '../../Icons'
 const Notion: React.FC = () => {
     const { token } = useAuth()
+    const [isLoading, setIsLoading] = useState<boolean>(false)
     const fetchAuthUrl = async () => {
-        const response = await generateOAuthURL(token) as {
-            authUrl
-            : string
+        setIsLoading(true)
+        try {
+            const response = await generateOAuthURL(token) as {
+                authUrl
+                : string
+            }
+            window.location.href = response.authUrl
         }
-        console.log("from notion", response)
-        window.location.href = response.authUrl
+        finally {
+            setIsLoading(false)
+        }
     }
 
     return (
-        <Button className='bg-green-800 hover:bg-green-600 rounded-md' onClick={fetchAuthUrl}>Notion</Button>
+        <Button className='bg-green-800 hover:bg-green-600 rounded-md' onClick={fetchAuthUrl}>{isLoading ? <Icons.spinner className='animate-spin h-4 w-4' /> : "Notion"}</Button>
     )
 }
 
--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
     GOOGLE_CLIENT_ID: "37607736898-3vvkm3ijlgbljkaodl5dldqms09muqlp.apps.googleusercontent.com",
-    API_URL: "http://localhost:3000"
+    API_URL: "http://localhost:8000"
 }
