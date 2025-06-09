88e7052bf57060e25c1a0e6821bad598f7a32d9c
Your Name <your.email@example.com>
2025-06-09 13:15:05 +0530

    add backend

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
     GOOGLE_CLIENT_ID: "37607736898-3vvkm3ijlgbljkaodl5dldqms09muqlp.apps.googleusercontent.com",
-    API_URL: "http://localhost:8000"
+    API_URL: "https://formly-9qxe.onrender.com"
 }
