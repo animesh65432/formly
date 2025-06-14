b4440937add36cbd3052d093d035b9eae8e40341
Your Name <your.email@example.com>
2025-06-14 17:24:01 +0530

    fix spelling

--- frontend/src/components/Preview/NotSelect/index.tsx
+++ frontend/src/components/Preview/NotSelect/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NotSelect: React.FC = () => {
     return (
         <div className='w-full h-full flex justify-center items-center '>
-            <p className=' text-sm md:text-xl lg:text-2xl text-green-800 font-bold underline'>did't select any Elements</p>
+            <p className=' text-sm md:text-xl lg:text-2xl text-green-800 font-bold underline'>didn't select any Elements</p>
         </div>
     )
 }
