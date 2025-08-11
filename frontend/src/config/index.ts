fa9c41ddb1d86cd7b8363d40358e6cb9c3085468
Your Name <your.email@example.com>
2025-08-11 09:20:04 +0530

    fix ui

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,6 +1,6 @@
 export const config = {
     GOOGLE_CLIENT_ID: "37607736898-o09rcn8a8ohf36t92aobuov695tq9qo4.apps.googleusercontent.com",
-    API_URL: "http://localhost:8000"
+    API_URL: "https://formly-3e1i.vercel.app"
 }
 
 // https://formly-9qxe.onrender.com
